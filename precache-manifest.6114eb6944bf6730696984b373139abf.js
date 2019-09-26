121ea1797ae4de3a4d5cd35c049342e89a7a56c0
Ti Jiang <jiang.ti1@husky.neu.edu>
2019-09-25 23:45:20 -0400

    init commit

---
+++ precache-manifest.6114eb6944bf6730696984b373139abf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bf5c771bd4bb8cfa4d84",
+    "url": "/static/css/main.de1d1038.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "faa292303957841fbc4a",
+    "url": "/static/js/2.e8f1f143.chunk.js"
+  },
+  {
+    "revision": "bf5c771bd4bb8cfa4d84",
+    "url": "/static/js/main.79a8c8b6.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "faa292303957841fbc4a",
+    "url": "/static/css/2.223f81d9.chunk.css"
+  },
+  {
+    "revision": "90e3a0312a30a7e74b8f90f779c22c55",
+    "url": "/index.html"
+  }
+];
